a62b94123ac2f34ca3b777012d072ee2697da130
ValentinoYikaLoza <138173621+ValentinoYikaLoza@users.noreply.github.com>
2024-11-10 15:31:40 -0500

    ADD: nuevos datos para las animaciones

--- lib/app/features/level_one/models/data.dart
+++ lib/app/features/level_one/models/data.dart
@@ -1,37 +1,57 @@
 // cada animacion debe venir con su propia velocidad y estado de bucle,
 // agregar eso como parámtros
-
 enum PlayerStates {
-  stay('sheets/player/stay.png', 4),
-  walk('sheets/player/walk.png', 8),
-  attack('sheets/player/attack.png', 4),
-  dance('sheets/player/dance.png', 10),
-  jump('sheets/player/jump.png', 5);
+  stay('sheets/player/stay.png', 4, true, 0.2),
+  walk('sheets/player/walk.png', 8, true, 0.1),
+  attack('sheets/player/attack.png', 4, false, 0.05),
+  dance('sheets/player/dance.png', 10, true, 0.2),
+  jump('sheets/player/jump.png', 5, true, 0.1);
 
   final String sheet;
   final int frames;
-  const PlayerStates(this.sheet, this.frames);
+  final bool loop;
+  final double fps;
+  const PlayerStates(
+    this.sheet,
+    this.frames,
+    this.loop,
+    this.fps,
+  );
 }
 
 enum ShadowStates {
-  sit('sheets/shadow/sit.png', 6),
-  walk('sheets/shadow/walk.png', 4),
-  bark('sheets/shadow/bark.png', 8);
+  sit('sheets/shadow/sit.png', 6, true, 0.2),
+  walk('sheets/shadow/walk.png', 4, true, 0.15),
+  bark('sheets/shadow/bark.png', 8, true, 0.1);
 
   final String sheet;
   final int frames;
-  const ShadowStates(this.sheet, this.frames);
+  final bool loop;
+  final double fps;
+  const ShadowStates(
+    this.sheet,
+    this.frames,
+    this.loop,
+    this.fps,
+  );
 }
 
 enum SpiderStates {
-  stay('sheets/spider/stay.png', 5),
-  walk('sheets/spider/walk.png', 4),
-  attack('sheets/spider/attack.png', 16),
-  die('sheets/spider/die.png', 17);
+  stay('sheets/spider/stay.png', 5, true, 0.1),
+  walk('sheets/spider/walk.png', 4, true, 0.1),
+  attack('sheets/spider/attack.png', 16, true, 0.08),
+  die('sheets/spider/die.png', 17, false, 0.05);
 
   final String sheet;
   final int frames;
-  const SpiderStates(this.sheet, this.frames);
+  final bool loop;
+  final double fps;
+  const SpiderStates(
+    this.sheet,
+    this.frames,
+    this.loop,
+    this.fps,
+  );
 }
 
 enum DoorStates {
