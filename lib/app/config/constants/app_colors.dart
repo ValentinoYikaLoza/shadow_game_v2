b4de66892ead3c331bb5f34d8f8075f461acfdf0
ValentinoYikaLoza <138173621+ValentinoYikaLoza@users.noreply.github.com>
2024-11-18 01:02:21 -0500

    ADD: new color

--- lib/app/config/constants/app_colors.dart
+++ lib/app/config/constants/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
+  static const Color golden = Color(0xFFe8c831);
 }
